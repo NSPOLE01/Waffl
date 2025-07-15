6f831b4f1ffe9d2b05f486ba41bdf82ece618657
NSPOLE01 <89700736+NSPOLE01@users.noreply.github.com>
2025-07-14 21:26:55 -0400

    initial commit

---
+++ Waffl/Waffl/WafflApp.swift
@@ -0,0 +1,17 @@
+//
+//  WafflApp.swift
+//  Waffl
+//
+//  Created by Nikhil Polepalli on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct WafflApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Waffl/WafflTests/WafflTests.swift
@@ -0,0 +1,17 @@
+//
+//  WafflTests.swift
+//  WafflTests
+//
+//  Created by Nikhil Polepalli on 7/14/25.
+//
+
+import Testing
+@testable import Waffl
+
+struct WafflTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
